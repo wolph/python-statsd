6c0018a44bddece58f5ea81e269e91367081f04a
ruslan.lutsenko <ruslan.lutcenko@gmail.com>
2012-04-04 16:44:32 +0200

    New gauge type added to statsd https://github.com/etsy/statsd/pull/62

--- statsd/__init__.py
+++ statsd/__init__.py
@@ -1,8 +1,9 @@
 from statsd.connection import Connection
 from statsd.client import Client
 from statsd.timer import Timer
+from statsd.gauge import Gauge
 from statsd.counter import Counter, increment, decrement
 
-__all__ = ['Client', 'Connection', 'Timer', 'Counter', 'increment',
+__all__ = ['Client', 'Connection', 'Timer', 'Counter', 'Gauge', 'increment',
     'decrement']
 
