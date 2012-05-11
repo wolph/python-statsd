32c611e0067ceca2180638032847cd0c842e69b1
WoLpH <Rick@Fawo.nl>
2012-05-11 23:47:41 +0200

    fixed small typo and pep8ified

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ else:
 
 setup(
     name = 'python-statsd',
-    version = '1.5.0',
+    version = '1.5.1',
     author = 'Rick van Hattem',
     author_email = 'Rick.van.Hattem@Fawo.nl',
     description = '''statsd is a client for Etsy's node-js statsd server. 
