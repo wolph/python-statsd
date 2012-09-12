ee1749c1865d48552aa53c5562aedc44be4ede89
Alfredo Deza <alfredodeza@gmail.com>
2012-09-12 12:48:58 -0400

    gauge tests

--- tests/test_gauge.py
+++ tests/test_gauge.py
@@ -1,19 +1,26 @@
 from __future__ import with_statement
+from unittest import TestCase
 from decimal import Decimal
 import mock
 import statsd
 
-with mock.patch('statsd.Client') as mock_client:
-    instance = mock_client.return_value
-    instance._send.return_value = 1
 
-    gauge = statsd.Gauge('testing')
-    gauge.send('', 10.5)
-    mock_client._send.assert_called_with(mock.ANY, {'testing': '10.5|g'})
+class TestGauge(TestCase):
 
-    gauge.send('', Decimal('6.576'))
-    mock_client._send.assert_called_with(mock.ANY, {'testing': '6.576|g'})
+    def setUp(self):
+        self.gauge = statsd.Gauge('testing')
 
-    gauge.send('', 1)
-    mock_client._send.assert_called_with(mock.ANY, {'testing': '1|g'})
+    def test_send_float(self):
+        with mock.patch('statsd.Client') as mock_client:
+            self.gauge.send('', 10.5)
+            mock_client._send.assert_called_with(mock.ANY, {'testing': '10.5|g'})
 
+    def test_send_decimal(self):
+        with mock.patch('statsd.Client') as mock_client:
+            self.gauge.send('', Decimal('6.576'))
+            mock_client._send.assert_called_with(mock.ANY, {'testing': '6.576|g'})
+
+    def test_send_integer(self):
+        with mock.patch('statsd.Client') as mock_client:
+            self.gauge.send('', 1)
+            mock_client._send.assert_called_with(mock.ANY, {'testing': '1|g'})
