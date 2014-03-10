f6d1e1c3d9ca4097137f41ed31203f8e862543f4
Rick van Hattem <Wolph@wol.ph>
2014-03-10 15:45:29 +0100

    made timer threshold configurable

--- statsd/__init__.py
+++ statsd/__init__.py
@@ -19,9 +19,9 @@ __all__ = [
 ]
 
 __package_name__ = 'python-statsd'
-__version__ = '1.6.2'
+__version__ = '1.6.3'
 __author__ = 'Rick van Hattem'
-__author_email__ = 'Rick.van.Hattem@Fawo.nl'
+__author_email__ = 'Wolph@wol.ph'
 __description__ = (
     '''statsd is a client for Etsy's node-js statsd server. '''
     '''A proxy for the Graphite stats collection and graphing server.''')
